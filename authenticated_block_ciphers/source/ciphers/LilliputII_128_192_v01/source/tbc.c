3911028dd8785ff7142eac86b246a6e55e3decd2
Kévin Le Gouguec <kevin.legouguec@airbus.com>
2018-12-19 17:45:16 +0100

    Add Lilliput-II 192 and 256

---
+++ authenticated_block_ciphers/source/ciphers/LilliputII_128_192_v01/source/tbc.c
@@ -0,0 +1,142 @@
+#include <stdint.h>
+#include <string.h>
+
+#include "constants.h"
+#include "parameters.h"
+#include "tbc.h"
+#include "tweakey.h"
+
+
+static void _state_init(uint8_t X[BLOCK_BYTES], const uint8_t message[BLOCK_BYTES])
+{
+    memcpy(X, message, BLOCK_BYTES);
+}
+
+
+static void _compute_round_tweakeys(
+    const uint8_t key[KEY_BYTES],
+    const uint8_t tweak[TWEAK_BYTES],
+    uint8_t RTK[ROUNDS][ROUND_TWEAKEY_BYTES]
+)
+{
+    uint8_t TK[TWEAKEY_BYTES];
+    tweakey_state_init(TK, key, tweak);
+    tweakey_state_extract(TK, 0, RTK[0]);
+
+    for (uint8_t i=1; i<ROUNDS; i++)
+    {
+        tweakey_state_update(TK);
+        tweakey_state_extract(TK, i, RTK[i]);
+    }
+}
+
+
+static void _nonlinear_layer(uint8_t X[BLOCK_BYTES], const uint8_t RTK[ROUND_TWEAKEY_BYTES])
+{
+    uint8_t F[ROUND_TWEAKEY_BYTES];
+    for (size_t j=0; j<ROUND_TWEAKEY_BYTES; j++)
+    {
+        F[j] = X[j] ^ RTK[j];
+    }
+
+    for (size_t j=0; j<ROUND_TWEAKEY_BYTES; j++)
+    {
+        F[j] = S[F[j]];
+    }
+
+    for (size_t j=0; j<8; j++)
+    {
+        size_t dest_j = 15-j;
+        X[dest_j] ^= F[j];
+    }
+}
+
+static void _linear_layer(uint8_t X[BLOCK_BYTES])
+{
+    X[15] ^= X[1];
+    X[15] ^= X[2];
+    X[15] ^= X[3];
+    X[15] ^= X[4];
+    X[15] ^= X[5];
+    X[15] ^= X[6];
+    X[15] ^= X[7];
+
+    X[14] ^= X[7];
+    X[13] ^= X[7];
+    X[12] ^= X[7];
+    X[11] ^= X[7];
+    X[10] ^= X[7];
+    X[9]  ^= X[7];
+}
+
+static void _permutation_layer(uint8_t X[BLOCK_BYTES], permutation p)
+{
+    if (p == PERMUTATION_NONE)
+    {
+        return;
+    }
+
+    uint8_t X_old[BLOCK_BYTES];
+    memcpy(X_old, X, BLOCK_BYTES);
+
+    const uint8_t *pi = PERMUTATIONS[p];
+
+    for (size_t j=0; j<BLOCK_BYTES; j++)
+    {
+        X[pi[j]] = X_old[j];
+    }
+}
+
+static void _one_round_egfn(uint8_t X[BLOCK_BYTES], const uint8_t RTK[ROUND_TWEAKEY_BYTES], permutation p)
+{
+    _nonlinear_layer(X, RTK);
+    _linear_layer(X);
+    _permutation_layer(X, p);
+}
+
+
+void lilliput_tbc_encrypt(
+    const uint8_t key[KEY_BYTES],
+    const uint8_t tweak[TWEAK_BYTES],
+    const uint8_t message[BLOCK_BYTES],
+    uint8_t ciphertext[BLOCK_BYTES]
+)
+{
+    uint8_t X[BLOCK_BYTES];
+    _state_init(X, message);
+
+    uint8_t RTK[ROUNDS][ROUND_TWEAKEY_BYTES];
+    _compute_round_tweakeys(key, tweak, RTK);
+
+    for (uint8_t i=0; i<ROUNDS-1; i++)
+    {
+        _one_round_egfn(X, RTK[i], PERMUTATION_ENCRYPTION);
+    }
+
+    _one_round_egfn(X, RTK[ROUNDS-1], PERMUTATION_NONE);
+
+    memcpy(ciphertext, X, BLOCK_BYTES);
+}
+
+void lilliput_tbc_decrypt(
+    const uint8_t key[KEY_BYTES],
+    const uint8_t tweak[TWEAK_BYTES],
+    const uint8_t ciphertext[BLOCK_BYTES],
+    uint8_t message[BLOCK_BYTES]
+)
+{
+    uint8_t X[BLOCK_BYTES];
+    _state_init(X, ciphertext);
+
+    uint8_t RTK[ROUNDS][ROUND_TWEAKEY_BYTES];
+    _compute_round_tweakeys(key, tweak, RTK);
+
+    for (uint8_t i=0; i<ROUNDS-1; i++)
+    {
+        _one_round_egfn(X, RTK[ROUNDS-1-i], PERMUTATION_DECRYPTION);
+    }
+
+    _one_round_egfn(X, RTK[0], PERMUTATION_NONE);
+
+    memcpy(message, X, BLOCK_BYTES);
+}
---
+++ authenticated_block_ciphers/source/ciphers/LilliputII_128_192_v01/source/tbc.h
@@ -0,0 +1,23 @@
+#ifndef CIPHER_H
+#define CIPHER_H
+
+#include <stdint.h>
+
+#include "parameters.h"
+
+
+void lilliput_tbc_encrypt(
+    const uint8_t key[KEY_BYTES],
+    const uint8_t tweak[TWEAK_BYTES],
+    const uint8_t message[BLOCK_BYTES],
+    uint8_t ciphertext[BLOCK_BYTES]
+);
+
+void lilliput_tbc_decrypt(
+    const uint8_t key[KEY_BYTES],
+    const uint8_t tweak[TWEAK_BYTES],
+    const uint8_t ciphertext[BLOCK_BYTES],
+    uint8_t message[BLOCK_BYTES]
+);
+
+#endif /* CIPHER_H */
