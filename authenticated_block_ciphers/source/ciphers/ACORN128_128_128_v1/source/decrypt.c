fa6fdf085cbf4af4b241e6b1dc03e8e11b6b4b54
Kévin Le Gouguec <kevin.legouguec@airbus.com>
2019-02-12 14:55:48 +0100

    [ACORN] Use "opt1" implementation from CAESAR package

--- authenticated_block_ciphers/source/ciphers/ACORN128_128_128_v1/source/decrypt.c
+++ authenticated_block_ciphers/source/ciphers/ACORN128_128_128_v1/source/decrypt.c
@@ -27,165 +27,241 @@
  */
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "cipher.h"
 #include "constants.h"
+#include "utils.h"
 
-#include <string.h>
-#include <stdlib.h>
+/*
+  This is the optimized implementation of ACORN-128.
+
+  In the implementation, we store the 293-bit register into 7 64-bit registers:
+  293-bit register R:  r292 r291 r290 r289 r288 r287 r286 r285 ...... r5 r4 r3 r2 r1 r0
+
+  state[0]:  r60  r59  r58  r57  ...... r2   r1   r0     (61 bits) (lsb: r0)
+  state[1]:  r106 r105 r104 r103 ...... r63  r62  r61    (46 bits) (lsb: r61)
+  state[2]:  r153 r152 r151 r150 ...... r109 r108 r107   (47 bits) (lsb: r107)
+  state[3]:  r192 r191 r190 r189 ...... r156 r155 r154   (39 bits) (lsb: r154)
+  state[4]:  r229 r228 r227 r226 ...... r195 r194 r193   (37 bits) (lsb: r193)
+  state[5]:  r288 r287 r286 r285 ...... r232 r231 r230   (59 bits) (lsb: r230)
+  state[6]:  r292 r291 r290 r289                         (4  bits) (lsb: r289)
+*/
 
 
-
-#define maj(x,y,z)     (  ((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z))  )
-#define ch(x,y,z)      (  ((x) & (y)) ^ ( ((x) ^ 1) & (z))  )
-
-//decrypt one bit
-void Decrypt_StateUpdate128_1bit(uint8_t *state, uint8_t *plaintextbit, uint8_t ciphertextbit, uint8_t *ks, uint8_t ca, uint8_t cb)
+//decrypt 32 bit.  This function is used when cb = 0
+void decrypt_32bits(uint64_t *state, uint32_t *plaintextword, uint32_t ciphertextword, uint32_t ca, uint32_t cb)
 {
-    uint32_t  j;
-    uint8_t f;
+    unsigned int f,ks;
+    uint64_t word_244, word_23, word_160, word_111, word_66, word_196;
+    uint64_t word_12,word_235;
 
-    state[289] ^= state[235] ^ state[230];
-    state[230] ^= state[196] ^ state[193];
-    state[193] ^= state[160] ^ state[154];
-    state[154] ^= state[111] ^ state[107];
-    state[107] ^= state[66]  ^ state[61];
-    state[61]  ^= state[23]  ^ state[0];
+    //f  = state[0] ^ (state[107] ^ 1) ^ maj(state[244], state[23], state[160]) ^ ch(state[230], state[111], state[66]) ^ (ca & state[196]) ^ (cb & (*ks));
 
-    f = FBK128(state, ks, ca, cb);
+    word_12  = state[0] >> 12;
+    word_235 = state[5] >> 5;
+    word_244 = state[5] >> 14;
+    word_23  = state[0] >> 23;
+    word_160 = state[3] >> 6;
+    word_111 = state[2] >> 4;
+    word_66  = state[1] >> 5;
+    word_196 = state[4] >> 3;
 
-    for (j = 0; j <= 291; j++) state[j] = state[j+1];
-    *plaintextbit = *ks ^ ciphertextbit;
-    state[292] = f ^ *plaintextbit;
+	state[6] ^= (state[5] ^ word_235) & 0xffffffff;
+	state[5] ^= (state[4] ^ word_196) & 0xffffffff;
+	state[4] ^= (state[3] ^ word_160) & 0xffffffff;
+	state[3] ^= (state[2] ^ word_111) & 0xffffffff;
+	state[2] ^= (state[1] ^ word_66)  & 0xffffffff;
+	state[1] ^= (state[0] ^ word_23)  & 0xffffffff;
+
+	/*
+    word_0   = state[0];
+    word_107 = state[2];
+    word_230 = state[5];
+    word_154 = state[3];
+    word_61  = state[1];
+    word_193 = state[4];
+    */
+
+	ks = word_12 ^ state[3] ^ maj(word_235, state[1], state[4]) ^ ch(state[5], word_111, word_66) ;
+	f = state[0] ^ (~state[2]) ^ maj(word_244, word_23, word_160) ^ (word_196 & ca) ^ (cb & ks);
+    *plaintextword = ciphertextword ^ ks;
+    f  = f ^ *plaintextword;
+	state[6] = state[6] ^ ( (uint64_t)f << 4 );
+
+
+    state[0] = (state[0] >> 32) | ((state[1] & 0xffffffff) << 29);  //32-(64-61) = 29
+    state[1] = (state[1] >> 32) | ((state[2] & 0xffffffff) << 14);  //32-(64-46) = 14
+    state[2] = (state[2] >> 32) | ((state[3] & 0xffffffff) << 15);  //32-(64-47) = 15
+    state[3] = (state[3] >> 32) | ((state[4] & 0xffffffff) << 7);   //32-(64-39) = 7
+    state[4] = (state[4] >> 32) | ((state[5] & 0xffffffff) << 5);   //32-(64-37) = 5
+    state[5] = (state[5] >> 32) | ((state[6] & 0xffffffff) << 27);  //32-(64-59) = 27
+    state[6] =  state[6] >> 32;
+
+    return;
 }
 
-// decrypt one byte
-void acorn128_dec_onebyte(uint8_t *state, uint8_t *plaintextbyte,
-       uint8_t ciphertextbyte, uint8_t *ksbyte, uint8_t cabyte, uint8_t cbbyte)
+//decrypt 32 bit.  This function is used when cb = 0
+void decrypt_32bits_fast(uint64_t *state, uint32_t *plaintextword, uint32_t ciphertextword, uint32_t ca)
 {
-    uint8_t i;
-    uint8_t plaintextbit,ciphertextbit, ks,ca,cb;
+    unsigned int f, ks;
+    uint64_t word_244, word_23, word_160, word_111, word_66, word_196;
+    uint64_t word_12,word_235;
 
-    *plaintextbyte = 0;
-    for (i = 0; i < 8; i++)
-    {
-        ca = (cabyte >> i) & 1;
-        cb = (cbbyte >> i) & 1;
-        ciphertextbit = (ciphertextbyte >> i) & 1;
-        Decrypt_StateUpdate128_1bit(state, &plaintextbit, ciphertextbit, &ks, ca, cb);
-        *plaintextbyte |= (plaintextbit << i);
-    }
+    //f  = state[0] ^ (state[107] ^ 1) ^ maj(state[244], state[23], state[160]) ^ ch(state[230], state[111], state[66]) ^ (ca & state[196]) ^ (cb & (*ks));
+
+    word_12  = state[0] >> 12;
+    word_235 = state[5] >> 5;
+    word_244 = state[5] >> 14;
+    word_23  = state[0] >> 23;
+    word_160 = state[3] >> 6;
+    word_111 = state[2] >> 4;
+    word_66  = state[1] >> 5;
+    word_196 = state[4] >> 3;
+
+	state[6] ^= (state[5] ^ word_235) & 0xffffffff;
+	state[5] ^= (state[4] ^ word_196) & 0xffffffff;
+	state[4] ^= (state[3] ^ word_160) & 0xffffffff;
+	state[3] ^= (state[2] ^ word_111) & 0xffffffff;
+	state[2] ^= (state[1] ^ word_66)  & 0xffffffff;
+	state[1] ^= (state[0] ^ word_23)  & 0xffffffff;
+
+	/*
+    word_0   = state[0];
+    word_107 = state[2];
+    word_230 = state[5];
+    word_154 = state[3];
+    word_61  = state[1];
+    word_193 = state[4];
+    */
+
+	ks = word_12 ^ state[3] ^ maj(word_235, state[1], state[4]) ^ ch(state[5], word_111, word_66) ;
+	f = state[0] ^ (~state[2]) ^ maj(word_244, word_23, word_160) ^ (word_196 & ca); // ^ (cb & *ks);
+    *plaintextword = ciphertextword ^ ks;
+    f  = f ^ *plaintextword;
+	state[6] = state[6] ^ ( (uint64_t)f << 4 );
+
+
+    state[0] = (state[0] >> 32) | ((state[1] & 0xffffffff) << 29);  //32-(64-61) = 29
+    state[1] = (state[1] >> 32) | ((state[2] & 0xffffffff) << 14);  //32-(64-46) = 14
+    state[2] = (state[2] >> 32) | ((state[3] & 0xffffffff) << 15);  //32-(64-47) = 15
+    state[3] = (state[3] >> 32) | ((state[4] & 0xffffffff) << 7);   //32-(64-39) = 7
+    state[4] = (state[4] >> 32) | ((state[5] & 0xffffffff) << 5);   //32-(64-37) = 5
+    state[5] = (state[5] >> 32) | ((state[6] & 0xffffffff) << 27);  //32-(64-59) = 27
+    state[6] =  state[6] >> 32;
+
+    return;
 }
 
 
-/* ------------------------------------ */
+void decrypt_8bits(uint64_t *state, uint32_t *plaintextword, uint32_t ciphertextword, uint32_t ca, uint32_t cb)
+{
+    unsigned int f, ks;
+    uint64_t word_244, word_23, word_160, word_111, word_66, word_196;
+    uint64_t word_12,word_235;
 
-int32_t crypto_aead_decrypt(
-	uint8_t *m, int32_t *mlen,
-	uint8_t *nsec,
-	const uint8_t *c, int32_t clen,
-	const uint8_t *ad, int32_t adlen,
+    word_12  = state[0] >> 12;
+    word_235 = state[5] >> 5;
+    word_244 = state[5] >> 14;
+    word_23  = state[0] >> 23;
+    word_160 = state[3] >> 6;
+    word_111 = state[2] >> 4;
+    word_66  = state[1] >> 5;
+    word_196 = state[4] >> 3;
+
+	state[6] ^= (state[5] ^ word_235) & 0xff;
+	state[5] ^= (state[4] ^ word_196) & 0xff;
+	state[4] ^= (state[3] ^ word_160) & 0xff;
+	state[3] ^= (state[2] ^ word_111) & 0xff;
+	state[2] ^= (state[1] ^ word_66)  & 0xff;
+	state[1] ^= (state[0] ^ word_23)  & 0xff;
+
+	ks = word_12 ^ state[3] ^ maj(word_235, state[1], state[4]) ^ ch(state[5], word_111, word_66);
+    ks &= 0xff;
+	f = state[0] ^ (~state[2]) ^ maj(word_244, word_23, word_160) ^ (word_196 & ca) ^ (cb & ks);
+    *plaintextword = ciphertextword ^ ks;
+    f  = (f ^ *plaintextword) & 0xff;
+	state[6] = state[6] ^ ( (uint64_t)f << 4 );
+
+    state[0] = (state[0] >> 8) | ((state[1] & 0xff) << (29+24));   //32-(64-61) = 29
+    state[1] = (state[1] >> 8) | ((state[2] & 0xff) << (14+24));   //32-(64-46) = 14
+    state[2] = (state[2] >> 8) | ((state[3] & 0xff) << (15+24));   //32-(64-47) = 15
+    state[3] = (state[3] >> 8) | ((state[4] & 0xff) << (7+24));    //32-(64-39) = 7
+    state[4] = (state[4] >> 8) | ((state[5] & 0xff) << (5+24));    //32-(64-37) = 5
+    state[5] = (state[5] >> 8) | ((state[6] & 0xff) << (27+24));    //32-(64-59) = 27
+    state[6] =  state[6] >> 8;
+
+    return;
+}
+
+
+//decrypt a message
+int crypto_aead_decrypt(
+	uint8_t *m,size_t *mlen,
+	const uint8_t *c,size_t clen,
+	const uint8_t *ad,size_t adlen,
 	const uint8_t *npub,
 	const uint8_t *k
 	)
 {
-  int32_t i;
-    uint8_t plaintextbyte, ciphertextbyte, ksbyte;
-    uint8_t state[293];
-    uint8_t tag[16];
-    uint8_t check = 0;
-    uint8_t ca, cb;
+    size_t i;
+    uint8_t tag[16],check=0;
+    uint32_t plaintextword, ciphertextword;
 
-    if (clen < 16) return -1;
+    uint64_t state[7];
+    uint32_t ca, cb;
 
     //initialization stage
-    acorn128_initialization(k, npub, state);
+    acorn128_initialization_32bitversion(k, npub, state);
 
     //process the associated data
-    for (i = 0; i < adlen; i++)
+    ca = 0xffffffff;
+    cb = 0xffffffff;
+    for (i = 0; i < adlen/4; i = i+1)
     {
-        acorn128_enc_onebyte(state, ad[i], &ciphertextbyte, &ksbyte, 0xff, 0xff);
+        encrypt_32bits(state, ((uint32_t*)ad)[i], &ciphertextword, ca, cb);
     }
 
-    for (i = 0; i < 256/8; i++)
+    for (i = adlen & (uint64_t)0xfffffffffffffffc; i < adlen; i++)
     {
-        if ( i == 0 ) plaintextbyte = 0x1;
-        else plaintextbyte = 0;
-
-        if ( i < 128/8)   ca = 0xff;
-        else ca = 0;
-
-        cb = 0xff;
-
-        acorn128_enc_onebyte(state, plaintextbyte, &ciphertextbyte, &ksbyte, ca, cb);
+        plaintextword = ad[i];
+        encrypt_8bits(state, plaintextword, &ciphertextword, ca, cb);
     }
 
-    //process the ciphertext
+    //256bits padding after the associated data
+    acorn128_padding_256(state,cb);
+
+    //process the plaintext
+    ca = 0xffffffff;
+    cb = 0;
     *mlen = clen - 16;
 
-    for (i = 0; i < *mlen; i++)
+    for (i = 0; i < *mlen/4; i=i+1)
     {
-        acorn128_dec_onebyte(state, &m[i], c[i], &ksbyte, 0xff, 0);
+        decrypt_32bits_fast(state, &(((uint32_t*)m)[i]), ((uint32_t*)c)[i], ca);  //&c[i], &ksword, ca, cb);
     }
 
-    for (i = 0; i < 256/8; i++)
+    for (i = *mlen & (uint64_t)0xfffffffffffffffc; i < *mlen; i++)
     {
-        if ( i == 0 ) plaintextbyte = 0x1;
-        else plaintextbyte = 0;
-
-        if ( i < 128/8)   ca = 0xff;
-        else ca = 0;
-
-        cb = 0;
-
-        acorn128_enc_onebyte(state, plaintextbyte, &ciphertextbyte, &ksbyte, ca, cb);
+        ciphertextword = c[i];
+        decrypt_8bits(state, &plaintextword, ciphertextword, ca, cb);
+        m[i] = plaintextword;
     }
+
+    //256 bits padding after the plaintext
+    acorn128_padding_256(state,cb);
 
     //finalization stage, we assume that the tag length is a multiple of bytes
-    acorn128_tag_generation(*mlen, adlen, 16, tag, state);
+    acorn128_tag_generation_32bits_version(tag, state);
 
     for (i = 0; i  < 16; i++) check |= (tag[i] ^ c[clen - 16 + i]);
     if (check == 0) return 0;
     else return -1;
 }
 
-
-
-uint8_t Decrypt(uint8_t *block, int32_t  mlen, uint8_t *key, uint8_t *npub,
- uint8_t *ad, int32_t  adlen, uint8_t *c, uint8_t *roundKeys)
+int Decrypt(uint8_t *block, size_t mlen, uint8_t *key, uint8_t *npub,
+ uint8_t *ad, size_t adlen, uint8_t *c, uint8_t *roundKeys)
 {
-	/* Add here the cipher decryption implementation */
-
-		static uint8_t *nsec;
-	nsec = malloc(CRYPTO_NSECBYTES);
-	
-	//length of inputs and param
-	int32_t clen = mlen + CRYPTO_ABYTES;
-	
-	uint8_t *AD;
-    AD = (uint8_t *) malloc(adlen * sizeof(uint8_t) );
-    memcpy(AD, ad, adlen);
-	
-	if(adlen !=16){
-	return crypto_aead_decrypt(
-	block, &mlen,
-	nsec,
-	c, clen,
-	AD, adlen,
-	npub,
-	key
-	);}
-	else if(adlen ==16){
-	return crypto_aead_decrypt(
-	block, &mlen,
-	nsec,
-	c, clen,
-	AD, adlen,
-	npub,
-	key
-	);
-	}
-	
+    return crypto_aead_decrypt(block, &mlen, c, mlen+CRYPTO_ABYTES, ad, adlen, npub, key);
 }
-
-
--- authenticated_block_ciphers/source/ciphers/ACORN128_128_128_v1/source/encrypt.c
+++ authenticated_block_ciphers/source/ciphers/ACORN128_128_128_v1/source/encrypt.c
@@ -27,255 +27,152 @@
  */
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "cipher.h"
 #include "constants.h"
+#include "utils.h"
 
-#include <string.h>
-#include <stdlib.h>
+/*
+  This is the optimized implementation of ACORN-128.
 
+  In the implementation, we store the 293-bit register into 7 64-bit registers:
+  293-bit register R:  r292 r291 r290 r289 r288 r287 r286 r285 ...... r5 r4 r3 r2 r1 r0
 
-/* -------------------------- */
+  state[0]:  r60  r59  r58  r57  ...... r2   r1   r0     (61 bits) (lsb: r0)
+  state[1]:  r106 r105 r104 r103 ...... r63  r62  r61    (46 bits) (lsb: r61)
+  state[2]:  r153 r152 r151 r150 ...... r109 r108 r107   (47 bits) (lsb: r107)
+  state[3]:  r192 r191 r190 r189 ...... r156 r155 r154   (39 bits) (lsb: r154)
+  state[4]:  r229 r228 r227 r226 ...... r195 r194 r193   (37 bits) (lsb: r193)
+  state[5]:  r288 r287 r286 r285 ...... r232 r231 r230   (59 bits) (lsb: r230)
+  state[6]:  r292 r291 r290 r289                         (4  bits) (lsb: r289)
+*/
 
-#define maj(x,y,z)     (  ((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z))  )
-#define ch(x,y,z)      (  ((x) & (y)) ^ ( ((x) ^ 1) & (z))  )
-
-uint8_t KSG128(uint8_t *state)
+// encrypt 32 bit;
+// used when cb = 0; so we can remove the operation (cb * ks)
+void encrypt_32bits_fast(uint64_t *state, uint32_t plaintextword, uint32_t *ciphertextword, uint32_t ca)
 {
-    return ( state[12] ^ state[154] ^ maj(state[235], state[61], state[193]) ^ ch(state[230], state[111], state[66]) );
-}
+    unsigned int f, ks;
+    uint64_t word_244, word_23, word_160, word_111, word_66, word_196;
+    uint64_t word_12,word_235;
 
-uint8_t mem;
-uint8_t FBK128(uint8_t *state, uint8_t *ks, uint8_t ca, uint8_t cb)
-{
-    uint8_t f;
-    *ks = KSG128(state);
-    f  = state[0] ^ (state[107] ^ 1) ^ maj(state[244], state[23], state[160]) ^ (ca & state[196]) ^ (cb & (*ks));
-    
-    return f;
-}
+	word_235 = state[5] >> 5;
+	word_196 = state[4] >> 3;
+	word_160 = state[3] >> 6;
+	word_111 = state[2] >> 4;
+	word_66  = state[1] >> 5;
+	word_23  = state[0] >> 23;
+    word_244 = state[5] >> 14;
+	word_12  = state[0] >> 12;
 
-//encrypt one bit
-void Encrypt_StateUpdate128_1bit00(uint8_t *state, uint8_t plaintextbit, uint8_t *ciphertextbit, uint8_t *ks, uint8_t cb)
-{
-    uint32_t  j;
-    uint8_t f;
-	uint8_t ca = 0x00;
-	
-    state[289] ^= state[235] ^ state[230];
-    state[230] ^= state[196] ^ state[193];
-    state[193] ^= state[160] ^ state[154];
-    state[154] ^= state[111] ^ state[107];
-    state[107] ^= state[66]  ^ state[61];
-    state[61]  ^= state[23]  ^ state[0];
-    
-	//mem= ca;
-    f  = FBK128(state, ks, ca, cb);
+    //update using those 6 LFSRs
+	state[6] ^= (state[5] ^ word_235) & 0xffffffff;
+	state[5] ^= (state[4] ^ word_196) & 0xffffffff;
+	state[4] ^= (state[3] ^ word_160) & 0xffffffff;
+	state[3] ^= (state[2] ^ word_111) & 0xffffffff;
+	state[2] ^= (state[1] ^ word_66)  & 0xffffffff;
+	state[1] ^= (state[0] ^ word_23)  & 0xffffffff;
 
-    for (j = 0; j <= 291; j++) state[j] = state[j+1];
-    state[292] = f ^ plaintextbit;
-    *ciphertextbit = *ks ^ plaintextbit;
-}
+	/*
+    word_0   = state[0];
+    word_107 = state[2];
+    word_230 = state[5];
+    word_154 = state[3];
+    word_61  = state[1];
+    word_193 = state[4];
+    */
 
-void Encrypt_StateUpdate128_1bit(uint8_t *state, uint8_t plaintextbit, uint8_t *ciphertextbit, uint8_t *ks, uint8_t ca, uint8_t cb)
-{
-    uint32_t  j;
-    uint8_t f;
-	//uint8_t ca = 0xff;
-	
-    state[289] ^= state[235] ^ state[230];
-    state[230] ^= state[196] ^ state[193];
-    state[193] ^= state[160] ^ state[154];
-    state[154] ^= state[111] ^ state[107];
-    state[107] ^= state[66]  ^ state[61];
-    state[61]  ^= state[23]  ^ state[0];
-    
-	//mem= ca;
-    f  = FBK128(state, ks, ca, cb);
+	//compute keystream
+	ks = word_12 ^ state[3] ^ maj(word_235, state[1], state[4]) ^ ch(state[5], word_111, word_66);
 
-    for (j = 0; j <= 291; j++) state[j] = state[j+1];
-    state[292] = f ^ plaintextbit;
-    *ciphertextbit = *ks ^ plaintextbit;
-}
+    //f  = state[0] ^ (state[107] ^ 1) ^ maj(state[244], state[23], state[160]) ^ ch(state[230], state[111], state[66]) ^ (ca & state[196]) ^ (cb & (*ks));
+	f = state[0] ^ (~state[2]) ^ maj(word_244, word_23, word_160) ^ (word_196 & ca);  // ^ (cb & *ks);
+    *ciphertextword = plaintextword ^ ks;
+	f = f ^ plaintextword;
+	state[6] = state[6] ^ ( (uint64_t)f << 4 );
 
-// encrypt one byte
-void acorn128_enc_onebyte(uint8_t *state, uint8_t plaintextbyte,
-       uint8_t *ciphertextbyte, uint8_t *ksbyte, uint8_t cabyte, uint8_t cbbyte)
-{
-    uint8_t i;
-    uint8_t plaintextbit,ciphertextbit,kstem,ca,cb;
+    //shift by 32 bits
+    state[0] = (state[0] >> 32) | ((state[1] & 0xffffffff) << 29);  //32-(64-61) = 29
+    state[1] = (state[1] >> 32) | ((state[2] & 0xffffffff) << 14);  //32-(64-46) = 14
+    state[2] = (state[2] >> 32) | ((state[3] & 0xffffffff) << 15);  //32-(64-47) = 15
+    state[3] = (state[3] >> 32) | ((state[4] & 0xffffffff) << 7);   //32-(64-39) = 7
+    state[4] = (state[4] >> 32) | ((state[5] & 0xffffffff) << 5);   //32-(64-37) = 5
+    state[5] = (state[5] >> 32) | ((state[6] & 0xffffffff) << 27);  //32-(64-59) = 27
+    state[6] =  state[6] >> 32;
 
-    *ciphertextbyte = 0;
-    kstem = 0;
-    *ksbyte = 0;
-    for (i = 0; i < 8; i++)
-    {
-        ca = (cabyte >> i) & 1;
-        cb = (cbbyte >> i) & 1;
-        plaintextbit = (plaintextbyte >> i) & 1;
-        
-        if(ca==0x00){
-        	Encrypt_StateUpdate128_1bit00(state, plaintextbit, &ciphertextbit, &kstem, cb);
-        }
-        else{
-        	Encrypt_StateUpdate128_1bit(state, plaintextbit, &ciphertextbit, &kstem, ca, cb);
-        }
-        *ciphertextbyte |= (ciphertextbit << i);
-        *ksbyte |= (kstem << i);
-    }
-    
-    
+    //perform encryption
+
+    return;
 }
 
 
-//The initialization state of ACORN
-/*The input to initialization is the 128-bit key; 128-bit IV;*/
-void acorn128_initialization(const uint8_t *key, const uint8_t *iv, uint8_t *state)
-{
-        int32_t i,j;
-        uint8_t m[293], ks, tem;
-
-        //initialize the state to 0
-        for (j = 0; j <= 292; j++) state[j] = 0;
-
-        //set the value of m
-        for (j = 0; j <=  15;   j++)   m[j] = key[j];
-        for (j = 16; j <= 31;   j++)   m[j] = iv[j - 16];
-        for (j = 32; j <= 223;  j++)   m[j] = key[j & 0xf];
-        m[32] ^= 1;
-
-        //run the cipher for 1792 steps
-        for (i = 0; i < 224; i++)
-        {
-             acorn128_enc_onebyte(state, m[i], &tem, &ks, 0xff, 0xff);
-        }
-}
-
-//the finalization state of acorn
-void acorn128_tag_generation(int32_t msglen, int32_t adlen, uint8_t maclen, uint8_t *mac, uint8_t *state)
-{
-    int32_t i;
-    uint8_t plaintextbyte  = 0;
-    uint8_t ciphertextbyte = 0;
-    uint8_t ksbyte = 0;
-
-    for (i = 0; i < 768/8; i++)
-    {
-        acorn128_enc_onebyte(state, plaintextbyte, &ciphertextbyte, &ksbyte, 0xff, 0xff);
-        if ( i >= (768/8 - 16) ) {mac[i-(768/8-16)] = ksbyte; }
-    }
-}
-
-
-/*------------------------------*/
-
-int32_t crypto_aead_encrypt(
-	uint8_t *c, int32_t *clen,
-	const uint8_t *m, int32_t mlen,
-	 uint8_t *ad, int32_t adlen,
-	const uint8_t *nsec,
+//encrypt a message
+void crypto_aead_encrypt(
+	uint8_t *c,size_t *clen,
+	const uint8_t *m,size_t mlen,
+	const uint8_t *ad,size_t adlen,
 	const uint8_t *npub,
-	uint8_t *k
+	const uint8_t *k
 	)
 {
-  	int32_t i;
-    uint8_t plaintextbyte, ciphertextbyte, ksbyte, mac[16];
-    uint8_t state[293];
-    uint8_t ca, cb;
-    
-    
+    size_t i;
+    uint8_t mac[16];
+    uint32_t plaintextword, ciphertextword;
+
+    uint64_t state[7];
+    uint32_t ca, cb;
 
     //initialization stage
-    acorn128_initialization(k, npub, state);
+    acorn128_initialization_32bitversion(k, npub, state);
 
     //process the associated data
-    for (i = 0; i < adlen; i++)
+    ca = 0xffffffff;
+    cb = 0xffffffff;
+    for (i = 0; i < adlen/4; i = i+1)
     {
-    	//ad[i] = 0x00;
-        acorn128_enc_onebyte(state, ad[i], &ciphertextbyte, &ksbyte, 0xff, 0xff);
+        encrypt_32bits(state, ((uint32_t*)ad)[i], &ciphertextword, ca, cb);
     }
 
-    for (i = 0; i < 32; i++)
+    // if adlen is not a multiple of 4, we process the remaining bytes
+    for (i = adlen & (uint64_t)0xfffffffffffffffc; i < adlen; i++)
     {
-        if ( i == 0 ) plaintextbyte = 0x1;
-        else plaintextbyte = 0;
-
-        if ( i < 16)   ca = 0xff;
-        else ca = 0;
-
-        cb = 0xff;
-
-        acorn128_enc_onebyte(state, plaintextbyte, &ciphertextbyte, &ksbyte, ca, cb);
+        plaintextword = ad[i];
+        encrypt_8bits(state, plaintextword, &ciphertextword, ca, cb);
     }
 
+    //256bits padding after the associated data
+    acorn128_padding_256(state,cb);
 
     //process the plaintext
-    for (i = 0; i < mlen; i++)
+    ca = 0xffffffff;
+    cb = 0;
+    for (i = 0; i < mlen/4; i=i+1)
     {
-        acorn128_enc_onebyte(state, m[i], &(c[i]), &ksbyte, 0xff, 0 );
+        encrypt_32bits_fast(state, ((uint32_t*)m)[i], &(((uint32_t*)c)[i]), ca);  //&c[i], &ksword, ca, cb);
     }
 
-    for (i = 0; i < 32; i++)
+    //if mlen is not a multiple of 32 bits, we process the remaining bytes.
+    for (i = mlen & (uint64_t)0xfffffffffffffffc; i < mlen; i++)
     {
-        if (i == 0) plaintextbyte = 0x1;
-        else plaintextbyte = 0;
-
-        if ( i < 16)   ca = 0xff;
-        else ca = 0;
-
-        cb = 0;
-
-        acorn128_enc_onebyte(state, plaintextbyte, &ciphertextbyte, &ksbyte, ca, cb);
+        plaintextword = m[i];
+        encrypt_8bits(state, plaintextword, &ciphertextword, ca, cb);
+        c[i] = ciphertextword;
     }
+
+    //256 bits padding after the plaintext
+    acorn128_padding_256(state,cb);
 
     //finalization stage, we assume that the tag length is a multiple of bytes
-    acorn128_tag_generation(mlen, adlen, 16, mac, state);
-
+    acorn128_tag_generation_32bits_version(mac, state);
     *clen = mlen + 16;
     memcpy(c+mlen, mac, 16);
-
-	
-    return 0;
 }
 
 
-/*--------------------------------*/
-
-void Encrypt(uint8_t *block, int32_t  mlen, uint8_t *key, uint8_t *npub,
- uint8_t *ad, int32_t  adlen, uint8_t *c, uint8_t *roundKeys)
+void Encrypt(uint8_t *block, size_t mlen, uint8_t *key, uint8_t *npub,
+ uint8_t *ad, size_t  adlen, uint8_t *c, uint8_t *roundKeys)
 {
-
-	static uint8_t *nsec;
-	nsec = malloc(CRYPTO_NSECBYTES);
-	
-	//length of inputs and param
-	int32_t clen = mlen + CRYPTO_ABYTES;
-	
-	uint8_t *AD;
-    AD = (uint8_t *) malloc(adlen * sizeof(uint8_t) );
-    memcpy(AD, ad, adlen);
-	
-	if(adlen !=16){
-	crypto_aead_encrypt(
-	c, &clen,
-	block, mlen,
-	AD, adlen,
-	nsec,
-	npub,
-	key
-	);}
-	else if(adlen ==16){
-	crypto_aead_encrypt(
-	c, &clen,
-	block, mlen,
-	AD, adlen,
-	nsec,
-	npub,
-	key
-	);
-	}
-	
+    size_t clen;
+    crypto_aead_encrypt(c, &clen, block, mlen, ad, adlen, npub, key);
 }
-
-
-
