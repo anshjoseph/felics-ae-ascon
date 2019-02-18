9cfbc6155b8b43cee9660ea7c69eb05f71ddcc88
Kévin Le Gouguec <kevin.legouguec@airbus.com>
2019-02-18 10:09:25 +0100

    [DEOXYS-I-128] Re-order include guards

--- authenticated_block_ciphers/source/ciphers/DEOXYSI128128_128_128_v1/source/encrypt.c
+++ authenticated_block_ciphers/source/ciphers/DEOXYSI128128_128_128_v1/source/encrypt.c
@@ -26,23 +26,17 @@
  *
  */
 
+#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "cipher.h"
 #include "constants.h"
 
 
-//#include "tweakableBC.h"
-
-#include <string.h>
-#include <stdlib.h>
-
 #define GETRCON(r) ( ((uint32_t)READ_RCON_BYTE(rcon[r])<<24) ^ ((uint32_t)READ_RCON_BYTE(rcon[r])<<16) ^ ((uint32_t)READ_RCON_BYTE(rcon[r])<<8) ^ ((uint32_t)READ_RCON_BYTE(rcon[r])<<0) )
 #define GETU32(pt) (((uint32_t)(pt)[0] << 24) ^ ((uint32_t)(pt)[1] << 16) ^ ((uint32_t)(pt)[2] <<  8) ^ ((uint32_t)(pt)[3]))
 #define PUTU32(ct, st) { (ct)[0] = (uint8_t)((st) >> 24); (ct)[1] = (uint8_t)((st) >> 16); (ct)[2] = (uint8_t)((st) >>  8); (ct)[3] = (uint8_t)(st); }
-
-
-
 
 
 /*
