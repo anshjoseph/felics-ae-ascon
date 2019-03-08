d9ac7a6c9cf6982ee5587ac0446be65cff420cc8
Kévin Le Gouguec <kevin.legouguec@airbus.com>
2019-03-08 14:03:03 +0100

    [Ascon] Add 64-bit variants of Ascon-128 and Ascon-128a

---
+++ authenticated_block_ciphers/source/ciphers/Ascon-128_vopt64/source/encrypt.c
@@ -0,0 +1,114 @@
+/*
+ *
+ * University of Luxembourg
+ * Laboratory of Algorithmics, Cryptology and Security (LACS)
+ *
+ * FELICS - Fair Evaluation of Lightweight Cryptographic Systems
+ *
+ * Copyright (C) 2015 University of Luxembourg
+ *
+ * Written in 2015 by Daniel Dinu <dumitru-daniel.dinu@uni.lu>
+ *
+ * This file is part of FELICS.
+ *
+ * FELICS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FELICS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cipher.h"
+#include "constants.h"
+#include "utils.h"
+
+
+static void crypto_aead_encrypt(
+    uint8_t *c, size_t *clen,
+    const uint8_t *m, size_t mlen,
+    const uint8_t *ad, size_t adlen,
+    const uint8_t *npub,
+    const uint8_t *k) {
+
+  uint64_t K0 = U64BIG(((uint64_t*)k)[0]);
+  uint64_t K1 = U64BIG(((uint64_t*)k)[1]);
+  uint64_t N0 = U64BIG(((uint64_t*)npub)[0]);
+  uint64_t N1 = U64BIG(((uint64_t*)npub)[1]);
+  uint64_t x0, x1, x2, x3, x4;
+  uint64_t t0, t1, t2, t3, t4;
+  uint64_t rlen;
+  size_t i;
+
+  // initialization
+  x0 = (uint64_t)((CRYPTO_KEYBYTES * 8) << 24 | (RATE * 8) << 16 | PA_ROUNDS << 8 | PB_ROUNDS << 0) << 32;
+  x1 = K0;
+  x2 = K1;
+  x3 = N0;
+  x4 = N1;
+  P12;
+  x3 ^= K0;
+  x4 ^= K1;
+
+  // process associated data
+  if (adlen) {
+    rlen = adlen;
+    while (rlen >= RATE) {
+      x0 ^= U64BIG(*(uint64_t*)ad);
+      P6;
+      rlen -= RATE;
+      ad += RATE;
+    }
+    for (i = 0; i < rlen; ++i, ++ad)
+      x0 ^= INS_BYTE(*ad, i);
+    x0 ^= INS_BYTE(0x80, rlen);
+    P6;
+  }
+  x4 ^= 1;
+
+  // process plaintext
+  rlen = mlen;
+  while (rlen >= RATE) {
+    x0 ^= U64BIG(*(uint64_t*)m);
+    *(uint64_t*)c = U64BIG(x0);
+    P6;
+    rlen -= RATE;
+    m += RATE;
+    c += RATE;
+  }
+  for (i = 0; i < rlen; ++i, ++m, ++c) {
+    x0 ^= INS_BYTE(*m, i);
+    *c = EXT_BYTE(x0, i);
+  }
+  x0 ^= INS_BYTE(0x80, rlen);
+
+  // finalization
+  x1 ^= K0;
+  x2 ^= K1;
+  P12;
+  x3 ^= K0;
+  x4 ^= K1;
+
+  // return tag
+  ((uint64_t*)c)[0] = U64BIG(x3);
+  ((uint64_t*)c)[1] = U64BIG(x4);
+  *clen = mlen + CRYPTO_KEYBYTES;
+}
+
+void Encrypt(uint8_t *block, size_t  mlen, uint8_t *key, uint8_t *npub,
+ uint8_t *ad, size_t  adlen, uint8_t *c)
+{
+    size_t clen;
+    crypto_aead_encrypt(c, &clen, block, mlen, ad, adlen, npub, key);
+}
---
+++ authenticated_block_ciphers/source/ciphers/Ascon-128a_vopt64/source/constants.h
@@ -0,0 +1,44 @@
+/*
+ *
+ * University of Luxembourg
+ * Laboratory of Algorithmics, Cryptology and Security (LACS)
+ *
+ * FELICS - Fair Evaluation of Lightweight Cryptographic Systems
+ *
+ * Copyright (C) 2015 University of Luxembourg
+ *
+ * Written in 2015 by Daniel Dinu <dumitru-daniel.dinu@uni.lu>
+ *
+ * This file is part of FELICS.
+ *
+ * FELICS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FELICS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include "data_types.h"
+
+
+#define BLOCK_SIZE 16
+#define KEY_SIZE 16
+
+#define CRYPTO_KEYBYTES KEY_SIZE
+#define CRYPTO_NPUBBYTES 16
+#define CRYPTO_ABYTES 16
+
+extern ROM_DATA_BYTE R[5][2];
+
+#endif /* CONSTANTS_H */
