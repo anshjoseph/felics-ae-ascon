f3ed73aee6735db0935776a87888884a4f7880f0
Kévin Le Gouguec <kevin.legouguec@airbus.com>
2019-03-29 11:03:54 +0100

    [scripts] Teach felics-publish to add custom captions and labels
    Also add scripts to generate some  of the tables used in Lilliput-AE's
    documentation.

---
+++ ci/README.md
@@ -0,0 +1,4 @@
+This folder contains scripts run by our Continuous Integration system
+to produce artifacts used in other projects, such as LaTeX tables for
+the NIST submission specification of Lilliput-AE, or HTML tables for
+paclido.fr.
---
+++ ci/publish-nist.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eux
+
+CI_DIR=$(dirname $0)
+FELICS_AE_DIR=${CI_DIR}/../FELICS-AE/authenticated_block_ciphers
+FELICS_PUBLISH=${FELICS_AE_DIR}/scripts/felics-publish
+FELICS_RESULTS=${FELICS_AE_DIR}/results
+
+mkdir -p ${CI_DIR}/results
+
+${FELICS_PUBLISH}                                                   \
+    ${FELICS_RESULTS}/lilliput-vs-caesar-usecase-1.json             \
+    -f 'cipher_name=.+128.?'                                        \
+    -c 'Performance results for 128-bit key algorithms on {arch}.'  \
+    -l 'table:bench-soft-128-{arch}'                                \
+    -o ${CI_DIR}/results/felics-128.tex
+
+${FELICS_PUBLISH}                                           \
+    ${FELICS_RESULTS}/lilliput-vs-caesar-usecase-1.json     \
+    -f 'cipher_name=Lilliput.+'                             \
+    -c 'Performance of \textsc{{Lilliput-AE}} on {arch}.'   \
+    -l 'table:bench-soft-lilliput-{arch}'                   \
+    -o ${CI_DIR}/results/felics-lilliput.tex
