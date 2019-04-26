6d716db9be42d2d17d143e72751d019f05195bbe
Kévin Le Gouguec <kevin.legouguec@airbus.com>
2019-04-26 11:08:11 +0200

    Implement tweakey update in assembly (2/N)
    Move  to  tweakey_state_update directly  with  the  hope of  improving
    register usage.   Replace memcpy  with word-sized moves;  the compiler
    insists on  using byte-sized  moves, even  when annotating  buffers as
    aligned.

--- FELICS-AE/authenticated_block_ciphers/source/ciphers/.templates/Lilliput_vmsp/tweakey.c
+++ FELICS-AE/authenticated_block_ciphers/source/ciphers/.templates/Lilliput_vmsp/tweakey.c
@@ -21,49 +21,6 @@ void tweakey_state_init(
 }
 
 
-#define NEWLINE "\n\t"
-
-static void _multiply_M(const uint8_t X[LANE_BYTES], uint8_t Y[LANE_BYTES])
-{
-    __asm__ volatile (
-        "mov.b 4(%[X]), r4" NEWLINE
-        "mov.b 5(%[X]), r5" NEWLINE
-        "mov.b 6(%[X]), r6" NEWLINE
-
-        "mov.b 2(%[X]), 3(%[Y])" NEWLINE
-        "mov.b 0(%[X]), 1(%[Y])" NEWLINE
-        "mov.b 7(%[X]), 0(%[Y])" NEWLINE
-
-        /* send x6 to y7 */
-        "mov.b r6, 7(%[Y])" NEWLINE
-        /* now compute y2 from x6 and x1 */
-        "rla.b r6" NEWLINE
-        "rla.b r6" NEWLINE
-        "xor.b 1(%[X]), r6" NEWLINE
-        "mov.b r6, 2(%[Y])" NEWLINE
-
-        /* send x5 to y6 */
-        "mov.b r5, 6(%[Y])" NEWLINE
-        /* now compute y5 from x5 and x4 */
-        "rla.b r5" NEWLINE
-        "rla.b r5" NEWLINE
-        "rla.b r5" NEWLINE
-        "xor.b r4, r5" NEWLINE
-        "mov.b r5, 5(%[Y])" NEWLINE
-
-        /* compute y4 from x4 and x3 */
-        "rra.b r4" NEWLINE
-        "rra.b r4" NEWLINE
-        "rra.b r4" NEWLINE
-        "xor.b 3(%[X]), r4" NEWLINE
-        "mov.b r4, 4(%[Y])" NEWLINE
-
-        : [Y] "=r" (Y)
-        : [X] "r" (X)
-        : "R4", "R5", "R6"
-    );
-}
-
 static void _multiply_M2(const uint8_t X[LANE_BYTES], uint8_t Y[LANE_BYTES])
 {
     uint8_t x15 = X[5]<<3 ^ X[4];
@@ -144,6 +101,8 @@ static void _multiply_MR3(const uint8_t X[LANE_BYTES], uint8_t Y[LANE_BYTES])
 #endif
 
 
+#define NEWLINE "\n\t"
+
 void tweakey_state_update(uint8_t TK[TWEAKEY_BYTES])
 {
     /* Skip lane 0, as it is multiplied by the identity matrix. */
@@ -154,8 +113,56 @@ void tweakey_state_update(uint8_t TK[TWEAKEY_BYTES])
 
     j = 1;
     TKj = TK + j*LANE_BYTES;
-    memcpy(TKj_old, TKj, LANE_BYTES);
-    _multiply_M(TKj_old, TKj);
+    __asm__ volatile (
+        "mov 0(%[Y]), 0(%[X])" NEWLINE
+        "mov 2(%[Y]), 2(%[X])" NEWLINE
+        "mov 4(%[Y]), 4(%[X])" NEWLINE
+        "mov 6(%[Y]), 6(%[X])" NEWLINE
+
+        "push r4" NEWLINE
+        "push r5" NEWLINE
+        "push r6" NEWLINE
+
+        "mov.b 4(%[X]), r4" NEWLINE
+        "mov.b 5(%[X]), r5" NEWLINE
+        "mov.b 6(%[X]), r6" NEWLINE
+
+        "mov.b 2(%[X]), 3(%[Y])" NEWLINE
+        "mov.b 0(%[X]), 1(%[Y])" NEWLINE
+        "mov.b 7(%[X]), 0(%[Y])" NEWLINE
+
+        /* Send x6 to y7. */
+        "mov.b r6, 7(%[Y])" NEWLINE
+        /* Now compute y2 from x6 and x1. */
+        "rla.b r6" NEWLINE
+        "rla.b r6" NEWLINE
+        "xor.b 1(%[X]), r6" NEWLINE
+        "mov.b r6, 2(%[Y])" NEWLINE
+
+        /* Send x5 to y6. */
+        "mov.b r5, 6(%[Y])" NEWLINE
+        /* Now compute y5 from x5 and x4. */
+        "rla.b r5" NEWLINE
+        "rla.b r5" NEWLINE
+        "rla.b r5" NEWLINE
+        "xor.b r4, r5" NEWLINE
+        "mov.b r5, 5(%[Y])" NEWLINE
+
+        /* Compute y4 from x4 and x3. */
+        "rra.b r4" NEWLINE
+        "rra.b r4" NEWLINE
+        "rra.b r4" NEWLINE
+        "bic.b #0b11100000, r4" NEWLINE
+        "xor.b 3(%[X]), r4" NEWLINE
+        "mov.b r4, 4(%[Y])" NEWLINE
+
+        "pop r6" NEWLINE
+        "pop r5" NEWLINE
+        "pop r4" NEWLINE
+        :
+        : [Y] "rm" (TKj), [X] "rm" (TKj_old)
+        : "R4", "R5", "R6", "memory"
+    );
 
     j = 2;
     TKj = TK + j*LANE_BYTES;
