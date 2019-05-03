anshjoseph/felics-ae-ascon

commit e5838b7b2e3364d39aeec9c3acdc62afac12ec7a

Update block index handling in tweak
So that it  matches recent changes in the  reference version.  Metrics
mostly went down, except for

- Lilliput-I ROM on MSP (+0.4%)
- Lilliput-*-{128,192} cycles on ARM (+0.2%)

FELICS-AE/authenticated_block_ciphers/source/ciphers/.templates/Lilliput_vfelicsref/i/lilliput-ae-i.h
@@ -13,32 +13,26 @@ static const uint8_t _0n[BLOCK_BYTES] = {
 static void _fill_msg_tweak(
     uint8_t       prefix,
     const uint8_t N[NONCE_BYTES],
-    uint64_t      block_nb,
+    size_t        block_index,
     uint8_t       tweak[TWEAK_BYTES]
 )
 {
-    /* The 192-bit tweak is filled as follows:
+    /* With an s-bit block index, the t-bit tweak is filled as follows:
      *
-     * - bits   1- 68: block number
-     *          1- 64: actual 64-bit block number
-     *         64- 68: 0-padding
-     * - bits  67-188: nonce
-     * - bits 189-192: constant 4-bit prefix
+     * - bits [      1, t-|N|-4]: block index
+     *        [      1,       s]: actual 64-bit block index
+     *        [    s+1, t-|N|-4]: 0-padding
+     * - bits [t-|N|-4,     t-4]: nonce
+     * - bits [    t-3,       t]: 4-bit prefix
      */
 
-    for (size_t i=0; i<sizeof(block_nb); i++)
-    {
-        uint64_t mask = (uint64_t)0xff << 8*i;
-        uint8_t b = (mask & block_nb) >> 8*i;
+    copy_block_index(block_index, tweak);
 
-        tweak[i] = b;
-    }
-
-    tweak[sizeof(block_nb)] = lower_nibble(N[0]) << 4;
+    tweak[sizeof(block_index)] = lower_nibble(N[0]) << 4;
 
     for (size_t i=1; i<NONCE_BYTES; i++)
     {
-        tweak[sizeof(block_nb)+i] = lower_nibble(N[i]) << 4 ^ upper_nibble(N[i-1]);
+        tweak[sizeof(block_index)+i] = lower_nibble(N[i]) << 4 ^ upper_nibble(N[i-1]);
     }
 
     tweak[TWEAK_BYTES-1] = prefix << 4 ^ upper_nibble(N[NONCE_BYTES-1]);

FELICS-AE/authenticated_block_ciphers/source/ciphers/.templates/Lilliput_vfelicsref/ae-common.h
@@ -71,26 +71,33 @@ static inline void pad10(size_t X_len, const uint8_t X[X_len], uint8_t padded[BL
     memcpy(padded+pad_len, X, X_len);
 }
 
+static inline void copy_block_index(size_t index, uint8_t tweak[TWEAK_BYTES])
+{
+    /* NB: little-endian architectures can simply use:
+     *     memcpy(tweak, &index, sizeof(index)); */
+    for (size_t i=0; i<sizeof(index); i++)
+    {
+        tweak[i] = index >> 8*i & 0xff;
+    }
+}
+
 static inline void fill_index_tweak(
-    uint8_t  prefix,
-    uint64_t block_index,
-    uint8_t  tweak[TWEAK_BYTES]
+    uint8_t prefix,
+    size_t  block_index,
+    uint8_t tweak[TWEAK_BYTES]
 )
 {
-    /* The t-bit tweak is filled as follows:
+    /* With an s-bit block index, the t-bit tweak is filled as follows:
      *
      * - bits [  1, t-4]: block index
-     *        [  1,  64]: actual 64-bit block index
-     *        [ 65, t-4]: 0-padding
-     * - bits [t-3,   t]: constant 4-bit prefix
+     *        [  1,   s]: actual block index
+     *        [s+1, t-4]: 0-padding
+     * - bits [t-3,   t]: 4-bit prefix
      */
 
-    for (size_t i=0; i<sizeof(block_index); i++)
-    {
-        tweak[i] = block_index >> 8*i & 0xff;
-    }
+    copy_block_index(block_index, tweak);
 
-    /* Assume padding bytes have already been memset to 0. */
+    /* Assume padding bytes have already been set to 0. */
 
     tweak[TWEAK_BYTES-1] |= prefix << 4;
 }

FELICS-AE/authenticated_block_ciphers/source/ciphers/.templates/Lilliput_vfelicsref/ii/lilliput-ae-ii.h
@@ -7,25 +7,38 @@
 
 static void _init_msg_tweak(const uint8_t tag[TAG_BYTES], uint8_t tweak[TWEAK_BYTES])
 {
-    /* The t-bit tweak is filled as follows:
+    /* With an s-bit block index, the t-bit tweak is filled as follows:
      *
      * - bits [  1, t-1]: tag + block index
-     *        [  1,  64]: tag[ 1.. 64] XOR block index
-     *        [ 65, t-1]: tag[65..t-1]
+     *        [  1,   s]: tag[1..s] XOR block index
+     *        [s+1, t-1]: tag[s+1..t-1]
      * - bit t: 1
+     *
+     * This function sets bits s+1 to t once and for all.
      */
 
-    memcpy(tweak+sizeof(uint64_t), tag+sizeof(uint64_t), TAG_BYTES-sizeof(uint64_t));
+    memcpy(tweak+sizeof(size_t), tag+sizeof(size_t), TAG_BYTES-sizeof(size_t));
     tweak[TWEAK_BYTES-1] |= 0x80;
 }
 
-static void _fill_msg_tweak(const uint8_t tag[TAG_BYTES], uint64_t block_index, uint8_t tweak[TWEAK_BYTES])
+static void _fill_msg_tweak(const uint8_t tag[TAG_BYTES], size_t block_index, uint8_t tweak[TWEAK_BYTES])
 {
-    /* Assume bits 65 to t-1 are set. */
+    /* With an s-bit block index, the t-bit tweak is filled as follows:
+     *
+     * - bits [  1, t-1]: tag + block index
+     *        [  1,   s]: tag[1..s] XOR block index
+     *        [s+1, t-1]: tag[s+1..t-1]
+     * - bit t: 1
+     *
+     * This function assumes bits s+1 to t have already been set, and
+     * only sets bits 1 to s.
+     */
+
+    copy_block_index(block_index, tweak);
+
     for (size_t i=0; i<sizeof(block_index); i++)
     {
-        uint8_t index_i = block_index >> i*8 & 0xff;
-        tweak[i] = tag[i] ^ index_i;
+        tweak[i] ^= tag[i];
     }
 }
 

commit ccfced22df69a2815a66a421ebe7c3f1fe16026b

Update ΘCB3 reference code to match paclido/sp3
Fix bug  in tweak construction  introduced in e5838b7;  optimize nonce
handling.

Performance changes:

On AVR:
    ROM: -11%
    RAM: +0.3% (1 byte)

On MSP:
    ROM: -17% (felicsref), -25% (msp)

On ARM:
    ROM: -15%

Marginal  (<1%) improvements  to  execution speed.   This is  expected
since  the  test vector  is  only  one-block-long: the  nonce-handling
optimization  ensures that  the nonce  is copied  into the  tweak once
instead of l times, so there should be no speedup when l=1.

FELICS-AE/authenticated_block_ciphers/source/ciphers/.templates/Lilliput_vfelicsref/i/decrypt.c
@@ -19,21 +19,21 @@ static void _decrypt_message(
     size_t rest = C_len % BLOCK_BYTES;
 
     RAM_DATA_BYTE tweak[TWEAK_BYTES];
-    RAM_DATA_BYTE checksum[BLOCK_BYTES];
+    _init_msg_tweak(N, tweak);
 
-    memset(tweak, 0, TWEAK_BYTES);
+    RAM_DATA_BYTE checksum[BLOCK_BYTES];
     memset(checksum, 0, BLOCK_BYTES);
 
     for (size_t j=0; j<l; j++)
     {
-        _fill_msg_tweak(0x0, N, j, tweak);
+        _fill_msg_tweak(0x0, j, tweak);
         decrypt(key, tweak, &C[j*BLOCK_BYTES], &M[j*BLOCK_BYTES]);
         xor_into(checksum, &M[j*BLOCK_BYTES]);
     }
 
     if (rest == 0)
     {
-        _fill_msg_tweak(0x1, N, l, tweak);
+        _fill_msg_tweak(0x1, l, tweak);
         encrypt(key, tweak, checksum, Final);
     }
     else
@@ -41,14 +41,14 @@ static void _decrypt_message(
         RAM_DATA_BYTE M_rest[BLOCK_BYTES];
         RAM_DATA_BYTE Pad[BLOCK_BYTES];
 
-        _fill_msg_tweak(0x4, N, l, tweak);
+        _fill_msg_tweak(0x4, l, tweak);
         encrypt(key, tweak, _0n, Pad);
         xor_arrays(rest, &M[l*BLOCK_BYTES], &C[l*BLOCK_BYTES], Pad);
 
         pad10(rest, &M[l*BLOCK_BYTES], M_rest);
         xor_into(checksum, M_rest);
 
-        _fill_msg_tweak(0x5, N, l+1, tweak);
+        _fill_msg_tweak(0x5, l+1, tweak);
         encrypt(key, tweak, checksum, Final);
     }
 }

FELICS-AE/authenticated_block_ciphers/source/ciphers/.templates/Lilliput_vfelicsref/i/encrypt.c
@@ -19,21 +19,21 @@ static void _encrypt_message(
     size_t rest = M_len % BLOCK_BYTES;
 
     RAM_DATA_BYTE tweak[TWEAK_BYTES];
-    RAM_DATA_BYTE checksum[BLOCK_BYTES];
+    _init_msg_tweak(N, tweak);
 
-    memset(tweak, 0, TWEAK_BYTES);
+    RAM_DATA_BYTE checksum[BLOCK_BYTES];
     memset(checksum, 0, BLOCK_BYTES);
 
     for (size_t j=0; j<l; j++)
     {
         xor_into(checksum, &M[j*BLOCK_BYTES]);
-        _fill_msg_tweak(0x0, N, j, tweak);
+        _fill_msg_tweak(0x0, j, tweak);
         encrypt(key, tweak, &M[j*BLOCK_BYTES], &C[j*BLOCK_BYTES]);
     }
 
     if (rest == 0)
     {
-        _fill_msg_tweak(0x1, N, l, tweak);
+        _fill_msg_tweak(0x1, l, tweak);
         encrypt(key, tweak, checksum, Final);
     }
     else
@@ -44,11 +44,11 @@ static void _encrypt_message(
         pad10(rest, &M[l*BLOCK_BYTES], M_rest);
         xor_into(checksum, M_rest);
 
-        _fill_msg_tweak(0x4, N, l, tweak);
+        _fill_msg_tweak(0x4, l, tweak);
         encrypt(key, tweak, _0n, Pad);
         xor_arrays(rest, &C[l*BLOCK_BYTES], &M[l*BLOCK_BYTES], Pad);
 
-        _fill_msg_tweak(0x5, N, l+1, tweak);
+        _fill_msg_tweak(0x5, l+1, tweak);
         encrypt(key, tweak, checksum, Final);
     }
 }

FELICS-AE/authenticated_block_ciphers/source/ciphers/.templates/Lilliput_vfelicsref/i/lilliput-ae-i.h
@@ -10,9 +10,38 @@ static const uint8_t _0n[BLOCK_BYTES] = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
+static void _init_msg_tweak(const uint8_t N[NONCE_BYTES], uint8_t tweak[TWEAK_BYTES])
+{
+    /* With an s-bit block index, the t-bit tweak is filled as follows:
+     *
+     * - bits [      1, t-|N|-4]: block index
+     *        [      1,       s]: actual block index
+     *        [    s+1, t-|N|-4]: 0-padding
+     * - bits [t-|N|-3,     t-4]: nonce
+     * - bits [    t-3,       t]: 4-bit prefix
+     *
+     * This function sets bits s+1 to t-4 once and for all.
+     */
+
+    size_t N_start = TWEAK_BYTES - NONCE_BYTES - 1;
+
+    for (size_t i=sizeof(size_t); i<N_start; i++)
+    {
+        tweak[i] = 0;
+    }
+
+    tweak[N_start] = lower_nibble(N[0]) << 4;
+
+    for (size_t i=1; i<NONCE_BYTES; i++)
+    {
+        tweak[N_start+i] = lower_nibble(N[i]) << 4 ^ upper_nibble(N[i-1]);
+    }
+
+    tweak[TWEAK_BYTES-1] = upper_nibble(N[NONCE_BYTES-1]);
+}
+
 static void _fill_msg_tweak(
     uint8_t       prefix,
-    const uint8_t N[NONCE_BYTES],
     size_t        block_index,
     uint8_t       tweak[TWEAK_BYTES]
 )
@@ -20,22 +49,20 @@ static void _fill_msg_tweak(
     /* With an s-bit block index, the t-bit tweak is filled as follows:
      *
      * - bits [      1, t-|N|-4]: block index
-     *        [      1,       s]: actual 64-bit block index
+     *        [      1,       s]: actual block index
      *        [    s+1, t-|N|-4]: 0-padding
-     * - bits [t-|N|-4,     t-4]: nonce
+     * - bits [t-|N|-3,     t-4]: nonce
      * - bits [    t-3,       t]: 4-bit prefix
+     *
+     * This function assumes bits s+1 to t-3 have already been set,
+     * and only sets bits 1 to s and t-3 to t.
      */
 
     copy_block_index(block_index, tweak);
 
-    tweak[sizeof(block_index)] = lower_nibble(N[0]) << 4;
+    uint8_t *msb = &tweak[TWEAK_BYTES-1];
+    *msb = prefix<<4 ^ lower_nibble(*msb);
 
-    for (size_t i=1; i<NONCE_BYTES; i++)
-    {
-        tweak[sizeof(block_index)+i] = lower_nibble(N[i]) << 4 ^ upper_nibble(N[i-1]);
-    }
-
-    tweak[TWEAK_BYTES-1] = prefix << 4 ^ upper_nibble(N[NONCE_BYTES-1]);
 }
 
 static void _generate_tag(