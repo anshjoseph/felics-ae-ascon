f0096fff457c0e66106e02d9d211c19de0a1545a
Kévin Le Gouguec <kevin.legouguec@airbus.com>
2019-06-05 14:54:09 +0200

    [msp][threshold] Cleanup some blunders

--- sp4.2-software-implementations/FELICS-AE/source/ciphers/.templates/Lilliput_vthreshold/constants.h
+++ sp4.2-software-implementations/FELICS-AE/source/ciphers/.templates/Lilliput_vthreshold/constants.h
@@ -1,15 +1,1 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-#include "data_types.h"
-#include "parameters.h"
-
-
-#define BLOCK_SIZE BLOCK_BYTES
-#define KEY_SIZE KEY_BYTES
-
-#define CRYPTO_NPUBBYTES NONCE_BYTES
-#define CRYPTO_ABYTES TAG_BYTES
-
-
-#endif /* CONSTANTS_H */
+../Lilliput_vfelicsref/constants.h
