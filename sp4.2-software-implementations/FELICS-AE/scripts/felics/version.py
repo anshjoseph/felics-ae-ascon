9c5cf6534b141fd5428ecd9a92966aa282473b6e
Kévin Le Gouguec <kevin.legouguec@airbus.com>
2019-06-28 15:14:59 +0200

    [scripts] Tweak felics.version module
    Remove global constant; add specific functions.
    
    Also tweak felics-archive to fallback on commit hash.
    
    Next  step: make  shell  scripts use  felics.version with  appropriate
    arguments.

--- sp4.2-software-implementations/FELICS-AE/scripts/felics/version.py
+++ sp4.2-software-implementations/FELICS-AE/scripts/felics/version.py
@@ -1,25 +1,61 @@
 from os import path
+from sys import argv
+
 
 try:
     import git
     _REPO = git.Repo(path=__file__, search_parent_directories=True)
+except:
+    _REPO = None
+
+
+def version():
+    if _REPO is None:
+        version_file = path.join(
+            path.dirname(path.realpath(__file__)),
+            path.pardir,
+            path.pardir,
+            'VERSION'
+        )
+
+        with open(version_file) as v:
+            return v.read()
+
+    prefix = 'felics-ae-v'
+    version = _REPO.git.describe(match=prefix+'*', always=True)
+    return version.replace(prefix, '')
+
+
+def branch():
+    if _REPO is None:
+        return None
 
     try:
-        VERSION = _REPO.git.symbolic_ref('HEAD', short=True)
-    except GitCommandError:
-        VERSION = _REPO.git.show(format='format:%h', no_patch=True)
+        return _REPO.git.symbolic_ref('HEAD', short=True)
+    except git.GitCommandError:
+        return 'DETACHED'
 
-except:
-    _VERSION_FILE = path.join(
-        path.dirname(path.realpath(__file__)),
-        path.pardir,
-        path.pardir,
-        'VERSION'
-    )
 
-    with open(_VERSION_FILE) as v:
-        VERSION = v.read()
+def commit():
+    if _REPO is None:
+        return None
+
+    return _REPO.git.show(format='format:%h', no_patch=True)
+
+
+def _main(arguments):
+    template = '{version}' 
+    if arguments:
+       template = arguments[0]
+
+    info = {
+        'commit': commit(),
+        'branch': branch(),
+        'version': version()
+    }
+    
+    print(template.format_map(info))
 
 
 if __name__ == '__main__':
-    print(VERSION)
+    _main(argv[1:])
