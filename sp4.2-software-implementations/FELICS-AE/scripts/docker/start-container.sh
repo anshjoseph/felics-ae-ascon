a8bf47c10efe8f132ab466aa8a9e9b5702390fa6
Kévin Le Gouguec <kevin.legouguec@airbus.com>
2019-07-16 15:42:03 +0200

    [docker] Tweak docker-create options
    About to add  one more option; setting  them up this way  will make it
    easier.

--- sp4.2-software-implementations/FELICS-AE/scripts/docker/start-container.sh
+++ sp4.2-software-implementations/FELICS-AE/scripts/docker/start-container.sh
@@ -5,12 +5,11 @@ set -eux
 docker_dir=$(dirname $(realpath $0))
 felics_dir=${docker_dir}/../..
 
-mount=$(paste -sd, <<EOF
-type=bind
-src=${felics_dir}
-dst=/home/felics/FELICS-AE
-EOF
+options=(
+    --mount=type=bind,src="${felics_dir}",dst=/home/felics/FELICS-AE
+    --name felics-ae
+    -it
 )
 
-docker create --mount="${mount}" --name felics-ae -it felics-ae
+docker create "${options[@]}" felics-ae
 docker start felics-ae
