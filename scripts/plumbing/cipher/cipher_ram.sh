ee8b88dd587caabbe34363a74e20b803af8db6bf
Kévin Le Gouguec <kevin.legouguec@airbus.com>
2019-09-27 15:05:24 +0200

    Fix typo
    Introduced in 1c16604bb39a11c971115676ea225d2d3f7d5a5a (nRF52840 MR).

--- scripts/plumbing/cipher/cipher_ram.sh
+++ scripts/plumbing/cipher/cipher_ram.sh
@@ -127,7 +127,7 @@ function simulate()
 			make -f ./../../../common/cipher.mk ARCHITECTURE=$SCRIPT_ARCHITECTURE $target_file &> $make_log_file
 
 			$JLINK_GDB_SERVER -USB -device cortex-m3 &> $simulator_output_file &
-			$NRF52840_GDB -x $command_file &> $gdb_output_file
+			$ARM_GDB -x $command_file &> $gdb_output_file
 
 			jlink_gdb_server_pid=$(ps aux | grep "JLinkGDBServer" | grep -v "grep" | tr -s ' ' | cut -d ' ' -f 2)
 			for pid in $jlink_gdb_server_pid
