99e6752e653767a34febb42a8a75eb861c726240
Kévin Le Gouguec <kevin.legouguec@airbus.com>
2020-04-28 12:03:57 +0200

    Update performance presentation scripts

--- ci/publish-nist.sh
+++ ci/publish-nist.sh
@@ -11,21 +11,22 @@ FELICS_RESULTS=${FELICS_AE_DIR}/results
 
 mkdir -p ${CI_DIR}/results
 
-${FELICS_PUBLISH}                                                   \
-    ${FELICS_RESULTS}/lilliput-vs-caesar-usecase-1.json             \
-    -f 'cipher_name=.+128.?'                                        \
-    -c 'Performance results for 128-bit key algorithms on {arch}.'  \
-    -l 'table:bench-soft-128-{arch}'                                \
+versions_128='8bitfast|opt.*|.*ref'
+${FELICS_PUBLISH}                                                           \
+    ${FELICS_RESULTS}/all.json                                              \
+    -f "cipher_name=(Ascon|ACORN|Lilliput.+128),version=${versions_128}"    \
+    -c 'Performance results for 128-bit key algorithms on {arch}.'          \
+    -l 'table:bench-soft-128-{arch}'                                        \
     -o ${CI_DIR}/results/implem-soft-128.tex
 
 ${FELICS_PUBLISH}                                           \
-    ${FELICS_RESULTS}/lilliput-vs-caesar-usecase-1.json     \
-    -f 'cipher_name=Lilliput.+'                             \
+    ${FELICS_RESULTS}/all.json                              \
+    -f 'cipher_name=Lilliput.+,version=felicsref'           \
     -c 'Performance of \textsc{{Lilliput-AE}} on {arch}.'   \
     -l 'table:bench-soft-lilliput-{arch}'                   \
     -i='-version'                                           \
     -o ${CI_DIR}/results/implem-soft-lilliput.tex
 
 ${CI_DIR}/publish-nist-ti.py                    \
-    ${FELICS_RESULTS}/lilliput-ref-vs-ti.json   \
+    ${FELICS_RESULTS}/all.json                  \
     ${CI_DIR}/results/implem-soft-ti.tex
