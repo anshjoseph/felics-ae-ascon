ce5504cd4b2adfcb303b4b1e39f0a3ab6e8bcd69
Kévin Le Gouguec <kevin.legouguec@gmail.com>
2021-06-03 19:03:17 +0200

    Add script to copy implementations from NIST releases

---
+++ scripts/add-implementations.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2021 Kévin Le Gouguec
+
+set -eux
+
+name=$1
+shift
+
+scripts_dir=$(realpath $(dirname $0))
+ciphers_dir="${scripts_dir}"/../source/ciphers
+integrator=$(getent passwd "${USER}" | cut -d':' -f5)
+
+for d
+do
+    version=$(basename "$d")
+    dst="${ciphers_dir}/${name}_v${version}"
+
+    mkdir -p "${dst}"/source
+    cp "$d"/* "${dst}"/source
+
+    cat <<EOF > "${dst}"/source/implementation.info
+ImplementationDescription: Reference implementation of ${name} integrated by ${integrator}
+ImplementationAuthors: TODO
+
+EncryptCode: encrypt!crypto_aead_decrypt
+DecryptCode: encrypt!crypto_aead_encrypt
+EOF
+
+    if test "$d" = ref
+    then
+        kat=$(dirname "$d")/LWC_AEAD_KAT_128_128.txt
+        vector_file="${dst}"/source/test_vectors.c
+
+        grep -A5 "Count = 545" "${kat}" |
+            tail -n5 |
+            "${scripts_dir}"/convert-vector.sh > "${vector_file}"
+    else
+        (
+            cd "${dst}"/source
+            ln -s ../../"${name}_vref"/source/test_vectors.c .
+        )
+    fi
+done
